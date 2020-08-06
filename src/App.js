559c3575a6bcd445a517bec2ed13ada48ac01260
hxxpn <nebojsa.sim2@gmail.com>
2020-08-06 11:14:38 +0200

    First upload
    First basic upload of files, work in progress.
    New addons are styles, multiple seach bars and columns

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Movie from "./components/movie";
+import "bootstrap/dist/css/bootstrap.min.css";
+import SearchBar from "./components/searchBar";
+// Main question, only one movie or a list???
+class App extends React.Component {
+  //Always initialized to an empty page
+  //No need for wrapper yet?
+  
+  
+  render() {
+    return (
+      
+        <SearchBar/>
+        
+      
+    );
+  }
+}
+
+export default App;
